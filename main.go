6758b2e774408872e54a393196d8b656257647f8
alastairruhm <alastairruhm@gmail.com>
2016-11-18 13:51:10 +0800

    allow URL param input

--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"regexp"
 	"strings"
+	"time"
+)
+
+const (
+	// IpipAPIURL is the ipip api url
+	IpipAPIURL = "http://freeapi.ipip.net/"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("param needed, an ip or url maybe")
-		os.Exit(1)
-	} else if len(os.Args) > 3 {
-		fmt.Println("param number wrong, only one is acceptable")
-		os.Exit(1)
-	}
-	ip := os.Args[1]
-	resp, err := http.Get("http://freeapi.ipip.net/" + ip)
+	input, err := parseInputParam(os.Args[1:])
 	if err != nil {
-		fmt.Printf("http client error %v", err)
+		fmt.Println(err)
+		os.Exit(-1)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	s := extractData(string(body))
-	fmt.Println(strings.Join(s, "-"))
+	ips, err := parseInputToTargetIPS(input)
+	for _, ip := range ips {
+		requestAPI(ip)
+		// avoid 403 when request free API
+		time.Sleep(time.Second)
+	}
 }
 
 // trimSlice trim cutset of element in slice
@@ -43,4 +48,84 @@ func extractData(src string) []string {
 	dest := strings.Trim(src, "[]")
 	destSlice := trimSlice(strings.Split(dest, ","), "\"")
 	return destSlice
+}
+
+// LookupIP mock test
+var LookupIP = net.LookupIP
+
+// lookupIP look up ip against an domain using net package
+// It returns a array of string refer to IPs(ipv4)
+func lookupIP(domain string) []string {
+	var k []string
+	ips, err := LookupIP(domain)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	// only return ipv4
+	for _, ip := range ips {
+		if ipv4 := ip.To4(); ipv4 != nil {
+			k = append(k, ip.String())
+		}
+	}
+	return k
+}
+
+// parseInputParam get the extract only 1 parameter
+// It returns the first input parameter
+func parseInputParam(input []string) (string, error) {
+	if len(input) < 1 {
+		return "", errors.New("lack param , an ip or url maybe")
+	} else if len(input) >= 2 {
+		// fmt.Println("param number wrong, only one is acceptable")
+		return "", errors.New("too many params, an ip or url maybe")
+	}
+	return input[0], nil
+}
+
+func parseInputToTargetIPS(input string) ([]string, error) {
+	if isIP(input) {
+		return []string{input}, nil
+	} else if isURL(input) {
+		u, _ := url.Parse(input)
+		var host string
+		if u.Host != "" {
+			host = u.Host
+		} else {
+			host = u.Path
+		}
+		fmt.Println("lookup host:", host)
+		return lookupIP(host), nil
+	} else {
+		return nil, errors.New("input format error, should be IP or domain")
+	}
+}
+
+// isIP judge whether the string  an ip or not
+func isIP(ip string) bool {
+	if m, _ := regexp.MatchString(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`, ip); !m {
+		return false
+	}
+	return true
+}
+
+// isDomain judge whether the string an domain or not
+func isURL(raw string) bool {
+	if m, _ := url.Parse(raw); m == nil {
+		return false
+	}
+	return true
+}
+
+// requestAPI
+func requestAPI(ip string) {
+	resp, err := http.Get(IpipAPIURL + ip)
+	if err != nil {
+		fmt.Printf("http client error %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	s := extractData(string(body))
+	location := strings.Join(s, "-")
+	fmt.Println(ip + " " + location)
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net"
 	"reflect"
 	"testing"
 )
@@ -69,5 +70,171 @@ func Test_extractData(t *testing.T) {
 				t.Errorf("extractData() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Test_lookupIP(t *testing.T) {
+	// mock net.LookupIP
+	mockNetLookupIP := LookupIP
+	defer func() { LookupIP = mockNetLookupIP }()
+
+	LookupIP = func(domain string) ([]net.IP, error) {
+		return []net.IP{[]byte{8, 8, 8, 8}}, nil
+	}
+
+	type args struct {
+		domain string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "test correct lookup", args: args{"test.host"}, want: []string{"8.8.8.8"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lookupIP(tt.args.domain); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("lookupIP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseInputParam(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{name: "test param lack", args: args{[]string{}}, want: "", wantErr: true},
+		{
+			"test param exceed",
+			args{
+				[]string{"1", "2"},
+			},
+			"",
+			true,
+		},
+		{
+			"test one param",
+			args{
+				[]string{"1"},
+			},
+			"1",
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseInputParam(tt.args.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseInputParam() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("parseInputParam() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseInputToTargetIPS(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+	// TODO
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseInputToTargetIPS(tt.args.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseInputToTargetIPS() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseInputToTargetIPS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isIP(t *testing.T) {
+	type args struct {
+		ip string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "test correct IP 0", args: args{"255.255.255.255"}, want: true},
+		{name: "test correct IP 1", args: args{"0.0.0.0"}, want: true},
+		{name: "test incorrect IP 0", args: args{"0.0"}, want: false},
+		{name: "test incorrect IP 1", args: args{"#"}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isIP(tt.args.ip); got != tt.want {
+				t.Errorf("isIP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isURL(t *testing.T) {
+	type args struct {
+		raw string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "test correct domain in general", args: args{"google.com"}, want: true},
+		{name: "test correct domain with sub-domain", args: args{"api.google.com"}, want: true},
+		{name: "test correct domain with protocal", args: args{"https://golang.org/pkg/net/url/#URL"}, want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isURL(tt.args.raw); got != tt.want {
+				t.Errorf("isDomain() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_requestAPI(t *testing.T) {
+	type args struct {
+		ip string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			requestAPI(tt.args.ip)
+		})
+	}
+}
+
+func Test_converStringToByteArray(t *testing.T) {
+	real := []byte("8888")
+	expect := []byte{'8', '8', '8', '8'}
+	if !reflect.DeepEqual(real, expect) {
+		t.Errorf("real = %v, expect %v", real, expect)
 	}
 }
